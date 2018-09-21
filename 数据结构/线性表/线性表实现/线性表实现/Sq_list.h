13fb29ed7024f07fd5870ead73965ae3eafe08b4
锐 朱 <nicesiri4268@126.com>
2018-09-21 13:27:38 +0800

    线性表顺序储存结构

--- 数据结构/线性表/线性表实现/线性表实现/Sq_list.h
+++ 数据结构/线性表/线性表实现/线性表实现/Sq_list.h
@@ -5,28 +5,36 @@ using namespace std;
 
 struct point //链表节点
 {
-	int database[100];
+	int *elem;
+	int base_size = 5;
+	int add_long = 5;
 	int length;
+	int database_long;
 };
 
 void Init_Sqlist(point&L)
 {
 	int a;
 
-	printf("请输入要创建的元素的个数:\t");
-
+	printf("请输入要创建的元素的个数: ");
 	cin >> a;
 
-	for (int i = 0; i < a; i++)
+	//L.elem = (int *)malloc(L.base_size * sizeof(int));//传统方法申请数量表
+	L.elem = new int[a];//new申请一个空间的话不需要名称直接写大小,即new int [20],而不是new int A[20];
 
+	/*for (int i = 0; i < a; i++)
 	{
-
-		printf("请输入第%d个元素\t", i + 1);
-
-		cin >> L.database[i];
-
+			printf("请输入第%d个元素\t", i + 1);
+			cin >> L.elem[i];
+			L.length++;
+	}*/
+	for (int i = 0; i < a; i++)
+	{
+		//快速赋值函数
+		L.elem[i] = i+1;
 		L.length++;
 	}
+	
 }
 
 void show(point&L)
@@ -38,7 +46,7 @@ void show(point&L)
 
 	for (i = 0; i<L.length; i++)
 
-		printf("%d\t", L.database[i]);
+		printf("%d\t", L.elem[i]);
 
 	printf("\n");
 
